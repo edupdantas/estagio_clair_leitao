c9da1c45227d673ae0baa359feb9792874d09e2d
duduzin <edu.obgemail@gmail.com>
2024-04-17 20:21:54 -0300

    FEAT adicionada a biblioteca de Styled Components e criação de um novo componente botão utilizando a biblioteca

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
-import "../index.css"
+import styled from 'styled-components'
 
 export default function Button({bgColor, destaque, ...rest}){
+    const Button = styled.button`
+        margin-right: 12px;
+        margin-top: 14px;
+        margin-left: 0;
+        font-size: 20px;
+        border: 0;
+        outline: none;
+        padding: 12px;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: all 0.5s;
+    `
 
     return (
-        <button style={{margin:0}} {...rest}
+        <Button style={{margin:0}} {...rest}
 
         />
     )
