642a65c4765746d5713b31de8041900a4d7f9f11
duduzin <edu.obgemail@gmail.com>
2024-04-17 19:57:43 -0300

    FEAT adicionada a função de POST para adicionar um filme a wishlist da TMDB

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -4,6 +4,7 @@ import api from "../../services/api";
 import "./filme-info.css";
 import {toast} from "react-toastify";
 import Button from "../../components/Button.jsx";
+import axios from "axios";
 
 function Filme(){
     const { id } = useParams();
@@ -11,14 +12,24 @@ function Filme(){
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        async function loadFilme(){
-            await api.get(`/movie/${id}`, {
-                params:{
-                    api_key: 'e8a413dffea0a3f636fee44439c41934',
-                    language: 'pt-BR',
-                }
-            })
+    const options = {
+        method: 'POST',
+        url: 'https://api.themoviedb.org/3/account/17667603/watchlist',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGE0MTNkZmZlYTBhM2Y2MzZmZWU0NDQzOWM0MTkzNCIsInN1YiI6IjYzZWJiMDU0OGU4NzAyMDA3YTFhMDlhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QNvFDw5CEeu_AI4Ro9NMCsLrJH8pPOlopyEaKH2KRps'
+        },
+        data: {media_type: 'movie', media_id: id, watchlist: true}
+    };
+
+    async function loadFilme(){
+        await api.get(`/movie/${id}`, {
+            params:{
+                api_key: 'e8a413dffea0a3f636fee44439c41934',
+                language: 'pt-BR',
+            }
+        })
             .then((response)=>{
                 setFilme(response.data);
                 setLoading(false);
@@ -26,10 +37,29 @@ function Filme(){
             .catch(()=>{
                 console.log('FILME NÃO ENCONTRADO')
                 navigate("/",{replace: true})
-            return;
+                return;
             })
-        }
+    }
 
+    async function adicionar(){
+        await axios.request(options).then((r)=>{
+            toast.success('Filme adicionado a watchlist do TMDB com sucesso!', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+            console.log(r)
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+
+    useEffect(()=>{
         loadFilme();
     },[navigate, id])
 
@@ -86,6 +116,9 @@ function Filme(){
 
             <div className="area-buttons flex">
                 <Button onClick={salvarFilme} style={{marginRight:"12px"}}>Salvar</Button>
+                <Button onClick={adicionar} style={{marginRight:"12px"}}>
+                    Adicionar a watchlist
+                </Button>
                 <Button>
                     <a target="blank" rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
                 </Button>
